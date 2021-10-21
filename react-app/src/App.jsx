5a5812b02b36fce4f803416567024253a94f8e78
Andrew Nagy <andrewnagynemr@gmail.com>
2021-10-21 12:15:09 +0200

    lecture 2 b

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,37 +1,38 @@
 import { useState, useEffect } from "react";
-import { Post } from "./components/Post";
 import axios from "axios";
+import { ViewPosts } from "./components/ViewPosts";
+import { AddPost } from "./components/AddPost";
 
 export const App = () => {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
-      setPosts(result.data);
-    });
-  }, [posts]);
-
-  const handleClick = () => {
-    const newPost = { title: "fifth post", body: "my fifth post body" };
-    setPosts([...posts, newPost]);
-  };
+  // useEffect(() => {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
+  //     setPosts(result.data);
+  //   });
+  // }, [posts]);
 
   const handleDelete = () => {
     setPosts([]);
   };
 
+  const addPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
   return (
     <div className="container">
-      <h1>Hello from the app</h1>
-      <button className="btn btn-warning m-2" onClick={handleClick}>
-        Add
-      </button>
-      <button className="btn btn-danger m-2" onClick={handleDelete}>
-        Delete
-      </button>
-      {posts.map((p) => (
-        <Post title={p.title} body={p.body} />
-      ))}
+      <div className="row">
+        <div className="col-6">
+          <AddPost addPost={addPost} />
+        </div>
+        <div className="col-6">
+          <ViewPosts
+            posts={posts}
+            onDelete={handleDelete}
+          />
+        </div>
+      </div>
     </div>
   );
 };
